48f6b67a19c1df955678998b2b529e93a9d9c19a
Asion001 <kmita2002@gmail.com>
2020-09-02 19:15:57 +0300

    Set up freamwork

---
+++ main.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.uix.button import Button
+from kivy.config import Config
+from kivy.storage.jsonstore import JsonStore
+
+store = JsonStore('data.json')
+
+
+import time
+
+class setup():
+    def __init__(self):
+        data = []
+        for num in range(1,int(self.io("Скільки уроків?")) + 1):
+            data.append(str(self.io(str(num) + " урок (в форматі 8:00)") ) )
+        
+        store.put("data", Lessons = data)
+        print(store.get("data"))
+    def io(self,q):
+        return input(str(q) + " ")
+
+class IncrediblyCrudeClock(Label):
+    def update(self, *args):
+        self.text = time.asctime()
+
+class TimeApp(App):
+    def build(self):
+        self.title = "Lessons alarm"
+        crudeclock = IncrediblyCrudeClock()
+        Clock.schedule_interval(crudeclock.update, 1)
+        crudeclock.set_top = True
+        return crudeclock
+
+if __name__ == "__main__":
+    print(store.count())
+    if store.count() == 0: setup()
+    TimeApp().run()
